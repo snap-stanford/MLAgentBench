43e73d8bc814460507d697b17d50f4232d8db7c5
Peter Yong Zhong <yongzhon@andrew.cmu.edu>
2023-11-09 09:27:51 +0000

    fix dockerfile

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,1 +1,15 @@
-../install.sh
+#/bin/bash
+
+source activate mlagentbench
+# auto-gpt
+# pip install -r Auto-GPT/requirements.txt
+
+# crfm api
+# pip install crfm-helm
+
+# ML dependencies
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+pip install -r requirements.txt
+pip install pyg_lib torch_scatter torch_sparse torch_cluster -f https://data.pyg.org/whl/torch-2.0.1+cu117.html
+pip install typing-inspect==0.8.0 typing_extensions==4.5.0
+pip install -U jax==0.4.6 jaxlib==0.4.6+cuda11.cudnn86 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
