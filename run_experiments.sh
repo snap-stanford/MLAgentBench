ac804bace82b8d2f0e584d00c71648bd618bc2af
q-hwang <qhwang@stanford.edu>
2024-03-03 21:00:21 -0800

    update scripts

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,40 +3,21 @@
 # example tasks to run
 all_tasks="cifar10 imdb"
 log_dir=final_exp_logs
+models="claude2.1 gpt-4-0125-preview gemini-pro"
 
-#### gpt4 experiments with pure openai API 
-for task in $all_tasks
-do 
-    bash multi_run_experiment.sh $log_dir/gpt4/$task $task 8 {0..7} --llm-name gpt-4 --max-steps 30 --edit-script-llm-name gpt-4 --fast-llm-name gpt-3.5-turbo
+for model in $models
+do
+    for task in $all_tasks
+    do  
+        bash multi_run_experiment.sh $log_dir/$model/$task $task 8 {0..7} --llm-name $model --edit-script-llm-name $model --fast-llm-name $model
+    done
 done
 
-for task in $all_tasks
-do 
-    bash multi_run_experiment.sh $log_dir/no_retrieval_gpt4/$task $task 8 {0..7} --llm-name gpt-4 --max-steps 30 --edit-script-llm-name gpt-4 --no-retrieval --fast-llm-name gpt-3.5-turbo
-done
-
-#### gpt4 experiments with crfm API + claude API as the fast model
-for task in $all_tasks
-do 
-    bash multi_run_experiment.sh $log_dir/gpt4/$task $task 8 {0..7} --llm-name openai/gpt-4-0314 --max-steps 30 --edit-script-llm-name openai/gpt-4-0314
-done
+# other agent variants
 
 for task in $all_tasks
 do 
-    bash multi_run_experiment.sh $log_dir/no_retrieval_gpt4/$task $task 8 {0..7} --llm-name openai/gpt-4-0314 --max-steps 30 --edit-script-llm-name openai/gpt-4-0314 --no-retrieval 
-done
-
-#### Claude experiments
-
-for task in $all_tasks
-do    
-    bash multi_run_experiment.sh $log_dir/claude/$task $task 8 {0..7} 
-done
-
-
-for task in $all_tasks
-do 
-    bash multi_run_experiment.sh $log_dir/no_retrieval/$task $task 8 {0..7}  --no-retrieval 
+    bash multi_run_experiment.sh $log_dir/retrieval/$task $task 8 {0..7}  --retrieval 
 done
 
 for task in $all_tasks
@@ -52,4 +33,3 @@ for task in $all_tasks
 do 
     bash multi_run_experiment.sh $log_dir/langchain/$task $task 8 {0..7}  --agent-type LangChainAgent 
 done
-
