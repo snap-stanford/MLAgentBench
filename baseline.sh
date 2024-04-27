79ef1718b18d6ede05b0c446104bca8f7124ea3a
Qian Huang <qhwang@stanford.edu>
2024-04-26 20:42:28 -0700

    Update baseline.sh

--- baseline.sh
+++ baseline.sh
@@ -4,8 +4,8 @@ all_tasks="cifar10"
 for task in $all_tasks
 do    
     # 3 runs sanity check
-    bash multi_run_experiment.sh final_exp_logs/sanity_check/$task $task 1 0 --agent_type Agent
-    bash multi_run_experiment.sh final_exp_logs/sanity_check/$task $task 1 0 --agent_type Agent
-    bash multi_run_experiment.sh final_exp_logs/sanity_check/$task $task 1 0 --agent_type Agent
+    bash multi_run_experiment.sh final_exp_logs/sanity_check/$task $task 1 0 --agent-type Agent
+    bash multi_run_experiment.sh final_exp_logs/sanity_check/$task $task 1 0 --agent-type Agent
+    bash multi_run_experiment.sh final_exp_logs/sanity_check/$task $task 1 0 --agent-type Agent
 done
 
